c21910c349ec5a381d614efb5caf2d3ef7f48711
Leslie Saucedo <lsaucedo310@gmail.com>
2022-09-06 15:26:24 -0700

    commit

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,17 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'eyvqcfxf5reja3nv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      username: 'yqi4qdemmc3y7wy3',
+      password: "o5jorckls4pfcz9d",
+      database: "cqburrjet8qu4m8z",
+      port: 3306,
+      dialect: 'mysql'
+    });
 
 module.exports = sequelize;
